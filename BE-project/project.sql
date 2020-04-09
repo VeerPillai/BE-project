f2522b425b1b81b50432102b24a13565a6dc6104
Parth Panchal <51913848+parthpanchal3399@users.noreply.github.com>
2020-04-09 12:19:19 +0530

    login signup db updated

--- BE-project/project.sql
+++ BE-project/project.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 08, 2020 at 07:08 PM
+-- Generation Time: Apr 09, 2020 at 08:37 AM
 -- Server version: 10.4.11-MariaDB
--- PHP Version: 7.3.13
+-- PHP Version: 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -30,10 +29,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `question` (
   `q_id` int(11) NOT NULL,
-  `q_name` varchar(200) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
+  `q_name` varchar(200) NOT NULL,
   `t_id` int(11) NOT NULL,
-  `answer` varchar(20) COLLATE utf8mb4_unicode_nopad_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_nopad_ci;
+  `answer` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -42,22 +41,22 @@ CREATE TABLE `question` (
 --
 
 CREATE TABLE `score` (
-  `u_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `t_id` int(11) NOT NULL,
   `date` date NOT NULL,
   `score` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_nopad_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `topics`
+-- Table structure for table `topic`
 --
 
-CREATE TABLE `topics` (
+CREATE TABLE `topic` (
   `t_id` int(11) NOT NULL,
-  `tname` varchar(50) COLLATE utf8mb4_unicode_nopad_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_nopad_ci;
+  `tname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -66,12 +65,20 @@ CREATE TABLE `topics` (
 --
 
 CREATE TABLE `user` (
-  `u_id` int(11) NOT NULL,
-  `name` varchar(200) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `email` varchar(50) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `uname` varchar(20) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `password` varchar(16) COLLATE utf8mb4_unicode_nopad_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_nopad_ci;
+  `user_id` int(11) NOT NULL,
+  `nname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `uname` varchar(50) NOT NULL,
+  `pwd` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `nname`, `email`, `uname`, `pwd`) VALUES
+(1, 'test1', 'test@test.com', 'test1', '$2y$10$GW7fbwG9wKwOMIFT9I.3juO9iAm7K16Krtc0jQwFmwX9QBh7hk.Da'),
+(2, 'test2', 'test2@test.com', 'test2', '$2y$10$MExhR0Qv5gtulppNjaLVuuAOZA0hwxqvpNMiMdxsVfT0ojT3madC6');
 
 --
 -- Indexes for dumped tables
@@ -81,26 +88,49 @@ CREATE TABLE `user` (
 -- Indexes for table `question`
 --
 ALTER TABLE `question`
-  ADD KEY `Fk_question` (`t_id`);
+  ADD PRIMARY KEY (`q_id`),
+  ADD KEY `fk_topicidinQuestion` (`t_id`);
 
 --
 -- Indexes for table `score`
 --
 ALTER TABLE `score`
-  ADD KEY `fk_score_topic` (`t_id`),
-  ADD KEY `fk_score_user` (`u_id`);
+  ADD KEY `fk_userid` (`user_id`),
+  ADD KEY `fk_topicid` (`t_id`);
 
 --
--- Indexes for table `topics`
+-- Indexes for table `topic`
 --
-ALTER TABLE `topics`
+ALTER TABLE `topic`
   ADD PRIMARY KEY (`t_id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`u_id`);
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `question`
+--
+ALTER TABLE `question`
+  MODIFY `q_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `topic`
+--
+ALTER TABLE `topic`
+  MODIFY `t_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -110,14 +140,14 @@ ALTER TABLE `user`
 -- Constraints for table `question`
 --
 ALTER TABLE `question`
-  ADD CONSTRAINT `Fk_question` FOREIGN KEY (`t_id`) REFERENCES `topics` (`T_Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_topicidinQuestion` FOREIGN KEY (`t_id`) REFERENCES `topic` (`t_id`);
 
 --
 -- Constraints for table `score`
 --
 ALTER TABLE `score`
-  ADD CONSTRAINT `fk_score_topic` FOREIGN KEY (`t_id`) REFERENCES `topics` (`T_Id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_score_user` FOREIGN KEY (`u_id`) REFERENCES `user` (`U_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_topicid` FOREIGN KEY (`t_id`) REFERENCES `topic` (`t_id`),
+  ADD CONSTRAINT `fk_userid` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
